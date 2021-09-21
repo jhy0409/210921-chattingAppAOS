246e1379beb66a7dcb12f6050b86b6b69e74226a
jhy0409 <inooph@naver.com>
2021-09-21 14:54:07 +0900

    안드로이드 채팅앱

---
+++ -/210921_AOS/app/src/main/java/com/jhy/chattingaos/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.jhy.chattingaos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+
+class SplashActivity : AppCompatActivity() {
+    private LinearLayout linearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
