d3140de8388e9662e9e54f01583fb93424e8b49f
jhy0409 <inooph@naver.com>
2021-09-21 15:57:05 +0900

    ㅇ - 4강

--- chatApp/app/src/main/java/com/jhy/chatapp/MainActivity.java
+++ chatApp/app/src/main/java/com/jhy/chatapp/MainActivity.java
@@ -1,23 +1,35 @@
 package com.jhy.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     EditText etId, etPassword;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         etId = (EditText)findViewById(R.id.etId);
-         etPassword = (EditText)findViewById(R.id.etPassword);
+        mAuth = FirebaseAuth.getInstance();
+        etId = (EditText)findViewById(R.id.etId);
+        etPassword = (EditText)findViewById(R.id.etPassword);
 
 
         Button btnLogin = (Button)findViewById(R.id.btnLogin);
@@ -45,8 +57,34 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this,"Please insert Password", Toast.LENGTH_LONG).show();
                     return;
                 }
-                Toast.makeText(MainActivity.this, "Email: "+stEmail+", stPassword : "+stPassword, Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this, "Email: "+stEmail+", stPassword : "+stPassword, Toast.LENGTH_LONG).show();
+                mAuth.createUserWithEmailAndPassword(stEmail, stPassword)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+//                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                                    updateUI(null);
+                                }
+                            }
+                        });
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
