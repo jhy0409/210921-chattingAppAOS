a250fa394de8bd83f2e5d2e0a5dc06385b67bebc
jhy0409 <inooph@naver.com>
2021-09-21 17:39:44 +0900

    ㅇ - 6강

--- chatApp/app/src/main/java/com/jhy/chatapp/ChatActivity.java
+++ chatApp/app/src/main/java/com/jhy/chatapp/ChatActivity.java
@@ -1,12 +1,18 @@
 package com.jhy.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class ChatActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,14 @@ public class ChatActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] myDataset = {"test1", "test2", "test3", "test4"};
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
