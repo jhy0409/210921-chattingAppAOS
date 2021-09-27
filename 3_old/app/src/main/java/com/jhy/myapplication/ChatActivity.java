ba28b0fc7657ae739a9b93ef1ff95d8055ff66c3
jhy0409 <inooph@naver.com>
2021-09-27 11:08:45 +0900

    ㅇ - old 5

--- 3_old/app/src/main/java/com/jhy/myapplication/ChatActivity.java
+++ 3_old/app/src/main/java/com/jhy/myapplication/ChatActivity.java
@@ -4,9 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Hashtable;
 
 public class ChatActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -14,10 +26,46 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     String[] myDataset = {"A", "B", "C"};
 
+    EditText etText;
+    Button btnSend;
+    String email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            email = user.getEmail();
+        }
+
+        etText = (EditText)findViewById(R.id.etText);
+        btnSend = (Button)findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stText = etText.getText().toString();
+                if (stText.equals("") || stText.isEmpty()) {
+                    Toast.makeText(ChatActivity.this, "내용을 입력해주세요", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ChatActivity.this, email+", "+stText, Toast.LENGTH_LONG).show();
+
+                    Calendar c = Calendar.getInstance();
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String formattedDate = df.format(c.getTime());
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef =database.getReference("chats").child(formattedDate);
+
+                    Hashtable<String,String> chat = new Hashtable<String, String>();
+                    chat.put("email", email);
+                    chat.put("text", stText);
+                    myRef.setValue(chat);
+                }
+            }
+        });
+
 
         Button btnFinish = (Button)findViewById(R.id.btnFinish);
         btnFinish.setOnClickListener(new View.OnClickListener() {
--- 3_old/app/src/main/java/com/jhy/myapplication/MainActivity.java
+++ 3_old/app/src/main/java/com/jhy/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etPassword;
     String stEmail;
     String stPassword;
+    ProgressBar pbLogin;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListner;
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        pbLogin = (ProgressBar)findViewById(R.id.pbLogin);
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListner = new FirebaseAuth.AuthStateListener() {
@@ -102,10 +105,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void userLogin(String email, String password) {
+        pbLogin.setVisibility(View.VISIBLE);
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        pbLogin.setVisibility(View.GONE);
 //                        if (task.isSuccessful()) {
 //                            // Sign in success, update UI with the signed-in user's information
 //                        Log.d(TAG, "\n\n🌈🌈🌈 signInWithCustomToken:success");
