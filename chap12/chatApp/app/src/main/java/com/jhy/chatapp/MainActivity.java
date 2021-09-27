000c863d2ab4f3ecc80776f6e3d05907c1027e7f
jhy0409 <inooph@naver.com>
2021-09-27 14:09:56 +0900

    코드복기

--- chap12/chatApp/app/src/main/java/com/jhy/chatapp/MainActivity.java
+++ chap12/chatApp/app/src/main/java/com/jhy/chatapp/MainActivity.java
@@ -100,24 +100,24 @@ public class MainActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
 //                Toast.makeText(MainActivity.this, "Email: "+stEmail+", stPassword : "+stPassword, Toast.LENGTH_LONG).show();
                 mAuth.createUserWithEmailAndPassword(stEmail, stPassword)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
+                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
 //                                    updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(MainActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
 //                                    updateUI(null);
-                                }
                             }
-                        });
+                        }
+                    });
             }
         });
     }
